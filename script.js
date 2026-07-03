887ef2ab12818f1267df7dedaa2f6c3d1489f31a
J0remy34 <jeremyrin2@gmail.com>
2026-07-03 12:34:35 -0400

    Pagina web completa AWS

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 function openPage(id) {
-
-    let pages = document.querySelectorAll(".page");
-
-    pages.forEach(p => p.style.display = "none");
-
+    document.querySelectorAll(".page").forEach(p => p.style.display = "none");
     document.getElementById(id).style.display = "block";
 }
 
 function goHome() {
-
-    let pages = document.querySelectorAll(".page");
-
-    pages.forEach(p => p.style.display = "none");
-
+    document.querySelectorAll(".page").forEach(p => p.style.display = "none");
     document.getElementById("home").style.display = "block";
+}
+
+/* comentarios */
+function addComment(section) {
+    let input = document.getElementById("input-" + section);
+    let container = document.getElementById("comments-" + section);
+
+    if (input.value.trim() === "") return;
+
+    let div = document.createElement("div");
+    div.className = "card";
+    div.innerText = "💬 " + input.value;
+
+    container.appendChild(div);
+
+    input.value = "";
 }
